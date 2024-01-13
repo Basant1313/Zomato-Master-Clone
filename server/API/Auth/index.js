509bdf7b16f2ab041fd2197b7eb5576864d87f12
Basant1313 <basantbhagat1@gmail.com>
2024-01-13 19:58:09 +0530

    .

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -18,22 +18,17 @@ Access   Public
 Method   POST
 */
 
-Router.post("/signup" , async (req , res) => {
+Router.post("/signup" , async(req , res) => {
     // it is the best practice to use try and catch for debugging
     // 500 - will throw all errors inside your code its like some internal server error . As we cannot display the error code to the entire world which leads to hacking in production level.
     try {
 
         const { email, password, fullname, phoneNumber} = req.body.credentials;
 
-        //check wheather email or phone number exists
 
-        const checkUserByEmail = await UserModel.findOne({email});
+        await UserModel.findEmailAndPhone (email , phoneNumber);
 
-        const checkUserByPhone = await UserModel.findOne({phoneNumber});
-
-        if(checkUserByEmail || checkUserByPhone){
-            return res.json({error: "User Already Exits "});
-        }
+        
 
         // hashing -> password encrption into hexcode;
         
@@ -42,18 +37,17 @@ Router.post("/signup" , async (req , res) => {
 
 
         //hashing and salting 
-        const bcryptSalt = await bcrypt.genSalt(8); // bcrypt will generally fetch our password and generate a salting of 8 iteration (hashed for 8 times).
+
+        // -> const bcryptSalt = await bcrypt.genSalt(8);
+        
+        // bcrypt will generally fetch our password and generate a salting of 8 iteration (hashed for 8 times).
 
 
-        const hashedPassword = await bcrypt.hash(password , bcryptSalt);
+        // -> const hashedPassword = await bcrypt.hash(password , bcryptSalt);
 
 
         //DB --> create query is used to create field inside ou database 
-        await UserModel.create({
-            // spreading our body because it contains alot of things using spread operator
-           ...req.body.credentials,
-            password : hashedPassword
-        });
+        await UserModel.create( req.body.credentials);
 
         // JWT Auth Token -> adding extra layer of security its for making transfer of information between different party very secured.
 
@@ -75,3 +69,8 @@ export default Router;
 // if we want high level of password security then very good website have 2 , 3 ,4 10, 15 encryption for our password so that it can save securely. This will increase the reload time it will decrease the performance by some level (very small -> milisec) 
 
 // so for encryption we are using bcrypt (npm package);
+
+
+// Method is basically creating an instance and the  use or access that instance. example: checkUserByEmail.ourMethods();
+
+// Static is basically we don't create instance instead  of that we directly use it. example : UserModel.ourStatic();
--- server/database/users/index.js
+++ server/database/users/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     fullname : {type: String, required: true },
@@ -11,4 +11,52 @@ const UserSchema = new mongoose.Schema({
     timestamps: true,
 });
 
+// use for reducing code in auth API.
+UserSchema.statics.findEmailAndPhone = async (email , phoneNumber) =>{
+    // check  whether the email exists
+
+    const checkUserByEmail = await UserModel.findOne({email});
+
+    // check whether the phoneNumber exists
+    const checkUserByPhone = await UserModel.findOne({phoneNumber});
+
+
+        // whenever we use the static we can't return instead of that we can throw a error.
+    if (checkUserByEmail || checkUserByPhone){
+        throw new Error("User Already Exist");
+    }
+
+    return false; // no User with this email or phoneNumber exists. 
+};
+
+// reduce code of hasing and salting in auth API. so for reducing that we have to use pre function inbuilt method
+
+// first parameter ("save") -> It is action we are going to take when we are going to take the action why we are going to take the action.
+
+// second parameter (function(next)) -> it tells what happen after the function like action has exected what are you going to do.
+
+// whenever i'm creating a new database and saving it then this pre function should be executed.
+UserSchema.pre("save" , function(next) {
+    const user = this; // this means the present schema
+
+
+     // password is not Modified
+    if(!user.isModified("password")) return next();// if exist is not modiefied means user has already have password(already existing user).
+
+    // generating the bcrypt salt 
+    bcrypt.genSalt(8 , (error , salt) => {
+            if(error) return next(error);
+
+            // hashing the password 
+            bcrypt.hash(user.password, salt, (error,hash) => {
+                if(error) return next(error);
+
+                // assigning hashed password
+                user.password = hash;
+                return next;
+            });
+    });
+
+
+});
 export const UserModel = mongoose.model("Users" , UserSchema)
